4fa790d12afe5265a21106bfc4212786271e7302
ajay yadav <2016meb1145@iitrpr.ac.in>
2021-05-02 12:35:35 +0530

    add and delete contact feature in the contact list

--- index.js
+++ index.js
@@ -2,13 +2,53 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+app.use(express.urlencoded());
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
+app.use('/static',express.static('static'))
+
 
 const port = 3000
 
+var contactList = [
+    {
+        name:'ajay',
+        contact : 12345
+    },
+    {
+        name:'sanjay',
+        contact : 198765
+    },
+    {
+        name:'jaya',
+        contact : 12986345
+    }
+]
+   
+app.post('/delete',(req,res)=>{
+    contactList=contactList.filter(item=>item.name !== req.body.person)
+    res.redirect('/')
+})
+
+app.post('/',(req,res)=>{
+    var person={
+        name:req.body.name,
+        contact:req.body.contact
+    }
+
+    contactList.push(person);
+    res.redirect('/')
+})
+
 app.get('/',(req,res)=>{
-    return res.render('home')
+    return res.render('home',{
+        title:"My Contacts List",
+        contact_list:contactList
+    })
+})
+
+app.get('/practice',(req,res)=>{
+    return res.render('practice',{title:"Playing with ejs"})
 })
 
 app.listen(port,()=>{
